8d0df84597b60c6419d4c69a77076a9444e4986e
Gary Lengkong <G.Lengkong@F5.com>
2017-07-31 16:14:22 -0700

    Added utility class and the ability to load state from store

--- examples/common/controller.js
+++ examples/common/controller.js
@@ -1,3 +1,82 @@
+var backend = {
+  getTodos: getTodos,
+  getInfo: getInfo
+}
+
+function getTodos(name) {
+  // Fetch todos from backend
+  return Promise.resolve([]);
+}
+
+function getInfo() {
+  // Fetch data from backend
+  return Promise.resolve({});
+}
+
+var util = {
+  getRouteParameters: getRouteParameters,
+  wrapFunction: wrapFunction,
+  wrapFunctions: wrapFunctions,
+  getController: getController,
+  getItemFromStore: getItemFromStore,
+  setItemInStore: setItemInStore
+}
+
+// This function should be moved to a utility class/object
+function getRouteParameters() {
+  var parameters = {};
+  decodeURIComponent(window.location.hash).split("&").map(function(parameter) {
+    var keyValuePair = parameter.split("=");
+    parameters[keyValuePair[0]] = keyValuePair[1];
+  });
+  return parameters;
+}
+
+// This function should be moved to a utility class/object
+/**
+ * Executes before function before and after function after
+ * executing functionToWrap
+ */
+function wrapFunction(functionToWrap, before, after, thisObject) {
+  return function () {
+    var args = Array.prototype.slice.call(arguments),
+        result;
+    if (before) before.apply(thisObject || this, args);
+    result = functionToWrap.apply(thisObject || this, args);
+    if (after) after.apply(thisObject || this, args);
+    return result;
+  }
+}
+
+// This function should be moved to a utility class/object
+function wrapFunctions(functionsToWrap, before, after, thisObject) {
+  var wrappedFunctions = {};
+  for (var i in functionsToWrap) {
+    wrappedFunctions[i] = wrapFunction(functionsToWrap[i], before, after, thisObject);
+  }
+  return wrappedFunctions;
+}
+
+// This function should be moved to a utility class/object
+function getController(state, data, handlers, update, constants, thisObject) {
+  return {
+    state: state,
+    data: data,
+    handlers: wrapFunctions(handlers, null, update, thisObject),
+    constants: constants
+  };
+}
+
+// This function should be moved to a utility class/function
+function getItemFromStore(key) {
+  return JSON.parse(sessionStorage.getItem(key));
+}
+
+// This function should be moved to a utility class/function
+function setItemInStore(key, value) {
+  return sessionStorage.setItem(key, JSON.stringify(value));
+}
+
 /**
  * Returns an object containing the state and handlers
  * to be connected to the view. Update will be called
@@ -5,23 +84,17 @@
  */
 function todoController(updateView) {
 
-  var state = {
-    filter: "all",
-    todos: [],
-    editedTodo: null,
-    filteredTodos: [],
-    remainingCount: 0,
-    completedCount: 0,
-    newTodo: '',
-    allChecked: false,
-    originalTodo: ''
-  };
-
   var constants = {
+    NAME_URL_PARAM: 'name',
+    STORE_KEY: 'todo',
     ALL_TODOS: 'all',
     ACTIVE_TODOS: 'active',
     COMPLETED_TODOS: 'completed',
   };
+
+  var state = getState();
+
+  var data = getData();
 
   var handlers = {
     addTodo: addTodo,
@@ -37,41 +110,69 @@ function todoController(updateView) {
     setNewTodo: setNewTodo
   };
 
-  return getController(state, handlers, update, constants, this);
+  return getController(state, data, handlers, update, constants, this);
 
-  // This function should be moved to a utility class/object
+  function getState() {
+    // Get name of object from route param and retrieve data from the backend
+    var routeParams = util.getRouteParameters();
+    var todosName = routeParams[constants.NAME_URL_PARAM];
+    if (todosName) {
+      return getStateForExistingTodos(todosName);
+    } else {
+      // If we are not loading existing object, get saved state from the client store
+      var storedState = getStateFromStore();
+      return storedState ? storedState : getInitialState();
+    }
+  }
+
+  function getStateForExistingTodos() {
+    var state = getInitialState();
+    state.isLoaded = false;
+    loadTodos(todosName);
+    return state;
+  }
+
+  function loadTodos() {
+    // Populate state based on data from backend
+    return backend.getTodos(todosName).then(function(todos) {
+      state.todos = todos;
+      state.isLoaded = true;
+    });
+  }
+
   /**
-   * Executes before function before and after function after
-   * executing functionToWrap
+   * Data is the object that contains information that are not related to the
+   * state of the UI and has to be reloaded every time a page is refreshed.
+   * For example: The list of pools for the default pool dropdown in virtual
+   * server page.
    */
-  function wrap(functionToWrap, before, after, thisObject) {
-    return function () {
-      var args = Array.prototype.slice.call(arguments),
-          result;
-      if (before) before.apply(thisObject || this, args);
-      result = functionToWrap.apply(thisObject || this, args);
-      if (after) after.apply(thisObject || this, args);
-      return result;
-    }
+  function getData() {
+    // Fetch backend data
+    loadData();
+    return {};
   }
 
-  // This function should be moved to a utility class/object
-  function wrapFunctions(functionsToWrap, before, after, thisObject) {
-    var wrappedFunctions = {};
-    for (var i in functionsToWrap) {
-      wrappedFunctions[i] = wrap(functionsToWrap[i], before, after, thisObject);
-    }
-    return wrappedFunctions;
+  function loadData() {
+    return backend.getInfo().then(function(info) {
+      data.info = info;
+    });
   }
 
-  // This function should be moved to a utility class/object
-  function getController(state, handlers, update, constants, thisObject) {
+  function getInitialState() {
     return {
-      state: state,
-      handlers: wrapFunctions(handlers, null, update, thisObject),
-      constants: constants
+      filter: constants.ALL_TODOS,
+      todos: [],
+      editedTodo: null,
+      filteredTodos: [],
+      remainingCount: 0,
+      completedCount: 0,
+      newTodo: '',
+      allChecked: false,
+      originalTodo: '',
+      isLoaded: true
     };
   }
+
 
   function setNewTodo(value) {
     state.newTodo = value;
@@ -140,9 +241,18 @@ function todoController(updateView) {
   function update() {
     updateFilteredTodos();
     updateCounts();
+    updateStateInStore();
     if (updateView) {
       updateView(state);
     }
+  }
+
+  function getStateFromStore() {
+    return util.getItemFromStore(constants.STORE_KEY);
+  }
+
+  function updateStateInStore() {
+    return util.setItemInStore(constants.STORE_KEY, state);
   }
 
   function updateCounts() {
