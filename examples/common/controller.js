291b6f48e89782e9e44f35ad050f9813659d6611
Gary Lengkong <G.Lengkong@F5.com>
2017-08-01 14:55:33 -0700

    Move out the util and backend functions

--- examples/common/controller.js
+++ examples/common/controller.js
@@ -1,79 +1,3 @@
-var backend = {
-  getTodos: getTodos,
-  getInfo: getInfo
-}
-
-function getTodos(name) {
-  // Fetch todos from backend
-  return Promise.resolve([]);
-}
-
-function getInfo() {
-  // Fetch data from backend
-  return Promise.resolve({});
-}
-
-var util = {
-  getRouteParameters: getRouteParameters,
-  wrapFunction: wrapFunction,
-  wrapFunctions: wrapFunctions,
-  getController: getController,
-  getItemFromStore: getItemFromStore,
-  setItemInStore: setItemInStore
-}
-
-function getRouteParameters() {
-  var parameters = {};
-  decodeURIComponent(window.location.hash).split("&").map(function(parameter) {
-    var keyValuePair = parameter.split("=");
-    parameters[keyValuePair[0]] = keyValuePair[1];
-  });
-  return parameters;
-}
-
-/**
- * Executes before function before and after function after
- * executing functionToWrap
- */
-function wrapFunction(functionToWrap, before, after, thisObject) {
-  return function () {
-    var args = Array.prototype.slice.call(arguments),
-        result;
-    if (before) before.apply(thisObject || this, args);
-    result = functionToWrap.apply(thisObject || this, args);
-    if (after) after.apply(thisObject || this, args);
-    return result;
-  }
-}
-
-function wrapFunctions(functionsToWrap, before, after, thisObject) {
-  var wrappedFunctions = {};
-  for (var i in functionsToWrap) {
-    wrappedFunctions[i] = wrapFunction(functionsToWrap[i], before, after, thisObject);
-  }
-  return wrappedFunctions;
-}
-
-function getController(state, data, handlers, update, constants, thisObject) {
-  // Call update to get computed states
-  update();
-
-  return {
-    state: state,
-    data: data,
-    handlers: wrapFunctions(handlers, null, update, thisObject),
-    constants: constants
-  };
-}
-
-function getItemFromStore(key) {
-  return JSON.parse(sessionStorage.getItem(key));
-}
-
-function setItemInStore(key, value) {
-  return sessionStorage.setItem(key, JSON.stringify(value));
-}
-
 /**
  * Returns an object containing the state and handlers
  * to be connected to the view. Update will be called
@@ -248,6 +172,38 @@ function todoController(updateView) {
     }
   }
 
+  function getStateFromStore() {
+    return util.getItemFromStore(constants.STORE_KEY);
+  }
+
+  function updateStateInStore() {
+    return util.setItemInStore(constants.STORE_KEY, state);
+  }
+
+  function updateCounts() {
+    state.remainingCount = state.todos.filter(function(todo) {
+      return !todo.completed;
+    }).length;
+    state.completedCount = state.todos.length - state.remainingCount;
+    state.allChecked = state.remainingCount === 0;
+  }
+
+  function updateFilteredTodos() {
+    if (state.filter === constants.ACTIVE_TODOS) {
+      state.filteredTodos = state.todos.filter(function(todo) {
+        return !todo.completed;
+      });
+    } else if (state.filter === constants.COMPLETED_TODOS) {
+      state.filteredTodos = state.todos.filter(function(todo) {
+        return todo.completed;
+      });
+    } else if (state.filter === constants.ALL_TODOS || state.filter === ''){
+      state.filteredTodos = state.todos;
+    }
+  }
+
+  // Optional getters for computed states (states that derive values from other states)
+
   function getRemainingCount() {
     return state.todos.filter(function(todo) {
       return !todo.completed;
@@ -278,33 +234,4 @@ function todoController(updateView) {
     }
   }
 
-  function getStateFromStore() {
-    return util.getItemFromStore(constants.STORE_KEY);
-  }
-
-  function updateStateInStore() {
-    return util.setItemInStore(constants.STORE_KEY, state);
-  }
-
-  function updateCounts() {
-    state.remainingCount = state.todos.filter(function(todo) {
-      return !todo.completed;
-    }).length;
-    state.completedCount = state.todos.length - state.remainingCount;
-    state.allChecked = state.remainingCount === 0;
-  }
-
-  function updateFilteredTodos() {
-    if (state.filter === constants.ACTIVE_TODOS) {
-      state.filteredTodos = state.todos.filter(function(todo) {
-        return !todo.completed;
-      });
-    } else if (state.filter === constants.COMPLETED_TODOS) {
-      state.filteredTodos = state.todos.filter(function(todo) {
-        return todo.completed;
-      });
-    } else if (state.filter === constants.ALL_TODOS || state.filter === ''){
-      state.filteredTodos = state.todos;
-    }
-  }
 }
